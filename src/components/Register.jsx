3657f3deced8e5cce8c200265f3097300b5bd27f
mohammedasimahmed <asim60999@gmail.com>
2024-01-02 02:02:50 +0530

    commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -53,6 +53,7 @@ const Register = () => {
                     Password
                   </label>
                   <input
+                  type="password"
                     id="password"
                     placeholder="••••••••"
                     class="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500focus:border-blue-500"
